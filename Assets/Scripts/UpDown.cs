03d394f6b193c425150567aca6f8a905bfe73010
xmota360 <aluno@S08PC02.local>
2016-05-25 19:32:42 +0100

    Paddle Movement Script added

---
+++ Assets/Scripts/UpDown.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpDown : MonoBehaviour {
+
+	private Vector3 currentPos;
+	public float stickVel = 0.1f;
+	// Use this for initialization
+	void Start () 
+	{
+		currentPos = new Vector3 (0,0,0);
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey ("up"))
+
+			currentPos.y = transform.position.y + 1*stickVel;
+
+		if (Input.GetKey ("down"))
+
+			currentPos.y = transform.position.y - 1*stickVel;
+
+		transform.position = currentPos;
+
+	}
+}
