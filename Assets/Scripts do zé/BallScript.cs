abc5eaba5c0a7ccb93b0cec0c0d6ec18b5baf8fc
Lethiel117 <jose.vieira.peres@gmail.com>
2016-06-03 17:37:40 +0100

    extra optimizações
    removed obsolete variables

--- Assets/Scripts do zé/BallScript.cs
+++ Assets/Scripts do zé/BallScript.cs
@@ -2,24 +2,15 @@
 using System.Collections;
 
 public class BallScript : MonoBehaviour {
-	public float rng;
-	public float gnr;
-	public float rngforme;
+
 	public float randommax = 360;
-	public float randommin = -90;
-	public float randomone = 2;
-	public float randomneg = -2;
+	public float randommin = -360;
 	public Vector2 randomgen;
 	private Rigidbody2D rbk;
 	public Vector2 currentpos;
 	// Use this for initialization
 	void Start () {
-//		rng = Random.Range (randommin, randommax);
-//		gnr = Random.Range (randommin, randommax);
-//		rngforme = Random.Range (randomneg, randomone);
-		Vector2 newVector = new Vector2(Random.Range(-360, 360), Random.Range(-360 ,360));
-//		randomgen.x = rng*rngforme;
-//		randomgen.y = gnr*rngforme;
+		Vector2 newVector = new Vector2(Random.Range(randommin, randommax), Random.Range(randommin ,randommax));
 		currentpos = transform.position;
 		rbk	= GetComponent<Rigidbody2D> ();
 		rbk.AddForceAtPosition (newVector,currentpos);
