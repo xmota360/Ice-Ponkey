daae60352d2b09c88851f305684861ba4c43bc94
PauloGamito <aluno@S08PC01.local>
2016-06-03 18:06:02 +0100

    Added Score

---
+++ Assets/Goal.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Goal : MonoBehaviour 
+{
+	// PUBLIC
+
+	public int score = 0;
+	public Text UIScore;
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.CompareTag ("Ball")) 
+		{
+			score++;
+			other.GetComponent<BallScript> ().SCORE();
+
+		}
+	}
+
+	void Update()
+	{
+		UIScore.text = "Score = " + score;
+	}
+}
