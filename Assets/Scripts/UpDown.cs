66c21e1ea95db05f528fd5604c414651d025ce3a
PauloGamito <aluno@S08PC01.local>
2016-06-03 17:43:58 +0100

    Applied limitation to paddle movement

--- Assets/Scripts/UpDown.cs
+++ Assets/Scripts/UpDown.cs
@@ -4,7 +4,12 @@ using System.Collections;
 public class UpDown : MonoBehaviour {
 
 	private Vector3 currentPos;
+	private Vector3 posLimit;
 	public float stickVel = 0.1f;
+
+	public float limitUp = 3.85f;
+	public float limitDown = -3.85f;
+
 	// Use this for initialization
 	void Start () 
 	{
@@ -24,5 +29,16 @@ public class UpDown : MonoBehaviour {
 
 		transform.position = currentPos;
 
+		if (transform.position.y > limitUp) 
+		{
+			posLimit = new Vector3 (transform.position.x, limitUp, transform.position.z);
+			transform.position = posLimit;
+		}
+
+		if (transform.position.y < limitDown)
+		{
+			posLimit = new Vector3 (transform.position.x, limitDown, transform.position.z);
+			transform.position = posLimit;
+		}
 	}
 }
